82431942416593f8f4799985a42e946a71bcbadf
pampam1511 <puplampupamela@gmail.com>
2026-05-11 17:30:38 +0100

    created the variables for the timer

--- frontend/game.js
+++ frontend/game.js
@@ -1,2 +1,8 @@
 const recipe = JSON.parse(localStorage.getItem("selectedRecipe"));
 if (!recipe) window.location.href = "cuisines.html";
+
+let timeLeft = recipe.time;
+let timerInterval = null;
+let stepsDone = new Array(recipe.steps.length).fill(false);
+let score = 0;
+let paused = false;
