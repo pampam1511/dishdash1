fc57e14e1620a7a23bc63c1f70a0ca935b707a4a
pampam1511 <puplampupamela@gmail.com>
2026-05-11 18:08:51 +0100

    created the timer

--- frontend/game.js
+++ frontend/game.js
@@ -10,3 +10,15 @@ let paused = false;
 document.getElementById("game-title").textContent = recipe.name;
 document.getElementById("game-meta").textContent =
   `${recipe.cuisine} · ${recipe.difficulty}`;
+
+function updatedTimer(){
+    const m = Math.floor(timeLeft/60);
+    const s = timeLeft % 60;
+    const el = document.getElementById('timer');
+    el.textContent = `${m}:{String(s).padStart(2,'0')}`;
+
+    if (timeLeft > recipe.time * 0.6) el.style.color = #a5fda2;
+    else if (timeLeft > recipe.time * 0.3) el.style.color = #FFE28A;
+    else el.style.color = #F87C63;
+
+}
